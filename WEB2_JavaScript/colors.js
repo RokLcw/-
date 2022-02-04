98d257ad5a4faa51510777a8afe2b520b38fc442
RokLcw <lcwlyw@naver.com>
2022-02-04 23:11:41 +0900

    생활코딩 WEB2-JavaScript

---
+++ WEB2_JavaScript/colors.js
@@ -0,0 +1,45 @@
+function nightDayHandler(self){ // 함수 선언 및 매개변수 전달
+    // var target = document.querySelector('body');    // body 태그를 가리키는 변수
+    // var alist = document.querySelectorAll('a');
+    var i=0;
+    
+    var links = {
+        setcolor: function(color) {
+            var alist = document.querySelectorAll('a');
+
+            while (i < alist.length) {
+            alist[i].style.color = color;
+            i = i+1;
+            }
+        }
+    }
+
+    var body = {
+        setColor: function (color) {
+            document.querySelector('body').style.color = color; 
+        },
+        setBackgroundColor: function (color) {
+            document.querySelector('body').style.backgroundColor = color;
+        }
+    }
+
+    if(self.value == 'night') {
+        body.setColor('white'); // 객체 사용
+        body.setBackgroundColor('black');
+        // target.style.backgroundColor='black';    // 속성(?) 사용
+        // target.style.color='white';
+        self.value = 'day'; // onclick 이벤트가 속해있는 input 태그를 가리킴
+
+        links.setcolor('powderblue');
+        // while (i < alist.length) {
+        //     alist[i].style.color = 'powderblue';
+        //     i = i+1;
+        // }
+    } else {
+        body.setColor('black');
+        body.setBackgroundColor('white');
+        self.value = 'night';
+
+        links.setcolor('black');
+    }
+}
